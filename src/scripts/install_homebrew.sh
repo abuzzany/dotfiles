6534c5516ca66f91f45420f6b4c8855851d690a8
Angel Buzany <eva@Angels-MacBook-Pro.local>
2024-09-14 11:45:51 -0600

    feat: improve errors

--- src/scripts/install_homebrew.sh
+++ src/scripts/install_homebrew.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 # Define color codes
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
@@ -6,17 +9,21 @@ NC='\033[0m' # No Color
 
 # Check if Homebrew is installed
 if ! command -v brew &> /dev/null; then
-    printf "${YELLOW}Installing Homebrew...${NC}\n"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo -e "${YELLOW}Installing Homebrew...${NC}"
+    if ! /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; then
+        echo -e "${RED}Failed to install Homebrew${NC}" >&2
+        exit 1
+    fi
 else
-    printf "${GREEN}Homebrew is already installed.${NC}\n"
+    echo -e "${GREEN}Homebrew is already installed.${NC}"
 fi
 
 # Check if Homebrew is in PATH
-if ! grep -q 'eval "$(/opt/homebrew/bin/brew shellenv)"' /Users/eva/.zprofile; then
-    printf "${YELLOW}Adding Homebrew to PATH...${NC}\n"
-    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/eva/.zprofile
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+BREW_PATH='/opt/homebrew/bin/brew'
+if ! grep -q "eval \"\$(${BREW_PATH} shellenv)\"" "${HOME}/.zprofile"; then
+    echo -e "${YELLOW}Adding Homebrew to PATH...${NC}"
+    echo "eval \"\$(${BREW_PATH} shellenv)\"" >> "${HOME}/.zprofile"
+    eval "$(${BREW_PATH} shellenv)"
 else
-    printf "${GREEN}Homebrew is already in PATH.${NC}\n"
+    echo -e "${GREEN}Homebrew is already in PATH.${NC}"
 fi
