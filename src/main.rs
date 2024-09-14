87153f6790f058f6f8a6d78f92289a9c5aef4360
Angel Buzany <eva@Angels-MacBook-Pro.local>
2024-09-13 23:42:14 -0600

    feat: add rust script to execute shell files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+use std::path::Path;
+use std::fs;
+use colored::*;
+
+fn main() -> std::io::Result<()> {
+   let scripts_dir = "./src/scripts";
+   let scripts = vec![
+        "install_brew_packages.sh"
+   ];
+   
+   for script in scripts {
+       execute_script(scripts_dir, script)?;
+   }
+
+   Ok(())
 }
+
+fn execute_script(scripts_dir: &str, script_name: &str) -> std::io::Result<()>  {
+    let script_path = Path::new(scripts_dir).join(script_name);
+    
+    if script_path.exists() {
+        println!("{}", format!("Executing {}...", script_name).blue());
+    
+        // Make sure the script is executable
+        Command::new("chmod")
+            .arg("+x")
+            .arg(&script_path)
+            .status()?;
+
+        // Execute the script
+        let output = Command::new("sh")
+            .arg("-c")
+            .arg(&script_path)
+            .output()?;
+
+        if output.status.success() {
+            println!("{}", format!("{} executed successfully.", script_name).green());
+            println!("{}", String::from_utf8_lossy(&output.stdout));
+        } else {
+            eprintln!("{}", format!("Error executing {}:", script_name).red());
+            eprintln!("{}", String::from_utf8_lossy(&output.stderr));
+        }
+    } else {
+        println!("{}", format!("Script {} not found.", script_name).yellow());
+    }
+
+    Ok(())
+}
+
